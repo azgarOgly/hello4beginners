8c0688ac0196941ff3578ca323006efa92f3be2a
Artur Zinatullin <artur.zinatullin@bigbank.ee>
2022-09-13 10:28:29 +0300

    Hello!

---
+++ src/main/java/az/test/Hello.java
@@ -0,0 +1,12 @@
+package az.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Hello {
+    public static void main(String[] args) {
+        log.info("Hello!");
+        log.info("Seems that you are all set.");
+        log.info("Meet you at Kääriku!");
+    }
+}
